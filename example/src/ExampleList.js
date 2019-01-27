0ea46446aa7444f05a5887d8673ce89cb7578911
Hatuey <>
2019-01-27 13:45:55 -0600

    removed many screens but two

--- example/src/ExampleList.js
+++ example/src/ExampleList.js
@@ -3,33 +3,8 @@
 import * as React from 'react';
 import { FlatList } from 'react-native';
 import { List, Divider, withTheme, type Theme } from 'react-native-paper';
-import AppbarExample from './AppbarExample';
-import AvatarExample from './AvatarExample';
-import BadgeExample from './BadgeExample';
-import BannerExample from './BannerExample';
 import BottomNavigationExample from './BottomNavigationExample';
-import ButtonExample from './ButtonExample';
 import CardExample from './CardExample';
-import CheckboxExample from './CheckboxExample';
-import ChipExample from './ChipExample';
-import DataTableExample from './DataTableExample';
-import DialogExample from './DialogExample';
-import DividerExample from './DividerExample';
-import FABExample from './FABExample';
-import IconButtonExample from './IconButtonExample';
-import ListAccordionExample from './ListAccordionExample';
-import ListSectionExample from './ListSectionExample';
-import ProgressBarExample from './ProgressBarExample';
-import RadioButtonExample from './RadioButtonExample';
-import RadioButtonGroupExample from './RadioButtonGroupExample';
-import SearchbarExample from './SearchbarExample';
-import SnackbarExample from './SnackbarExample';
-import SurfaceExample from './SurfaceExample';
-import SwitchExample from './SwitchExample';
-import TextExample from './TextExample';
-import TextInputExample from './TextInputExample';
-import ToggleButtonExample from './ToggleButtonExample';
-import TouchableRippleExample from './TouchableRippleExample';
 
 type Props = {
   theme: Theme,
@@ -37,33 +12,8 @@ type Props = {
 };
 
 export const examples = {
-  appbar: AppbarExample,
-  avatar: AvatarExample,
-  badge: BadgeExample,
-  banner: BannerExample,
   bottomNavigation: BottomNavigationExample,
-  button: ButtonExample,
   card: CardExample,
-  checkbox: CheckboxExample,
-  chip: ChipExample,
-  dataTable: DataTableExample,
-  dialog: DialogExample,
-  divider: DividerExample,
-  fab: FABExample,
-  iconButton: IconButtonExample,
-  listAccordion: ListAccordionExample,
-  listSection: ListSectionExample,
-  progressbar: ProgressBarExample,
-  radio: RadioButtonExample,
-  radioGroup: RadioButtonGroupExample,
-  searchbar: SearchbarExample,
-  snackbar: SnackbarExample,
-  surface: SurfaceExample,
-  switch: SwitchExample,
-  text: TextExample,
-  textInput: TextInputExample,
-  toggleButton: ToggleButtonExample,
-  touchableRipple: TouchableRippleExample,
 };
 
 const data = Object.keys(examples).map(id => ({ id, data: examples[id] }));
